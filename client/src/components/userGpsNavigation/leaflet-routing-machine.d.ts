8e903c10ea71e899b746dc8a6bbdd431e0b367ee
Giftmasil <masilagift@gmail.com>
2024-07-13 12:07:16 +0300

    Update leaflet-routing-machine.d.ts

--- client/src/components/userGpsNavigation/leaflet-routing-machine.d.ts
+++ client/src/components/userGpsNavigation/leaflet-routing-machine.d.ts
@@ -1,8 +1,11 @@
-//import * as L from 'leaflet';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as L from 'leaflet';
 import 'leaflet-routing-machine';
 
-declare module 'leaflet-routing-machine' {
-  interface RoutingControlOptions {
-    serviceUrl?: string;
-  }
+declare module 'leaflet' {
+    namespace Routing {
+        interface RoutingControlOptions {
+            serviceUrl?: string;
+        }
+    }
 }
