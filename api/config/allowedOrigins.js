0ef3efa4626de9bc07563a4ee2f21c7d8a6ab55a
Giftmasil <masilagift@gmail.com>
2024-07-14 20:11:53 +0300

    Update allowedOrigins.js

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     "https://teal-griffin-cf785c.netlify.app",
     "http://localhost:5173/",
+    "https://final-group-3-project.vercel.app/#/login",
     'https://final-group-3-project.onrender.com',
     'https://final-group-3-project.onrender.com/',
     'https://chat-bot-group-3.onrender.com/get_advice'
